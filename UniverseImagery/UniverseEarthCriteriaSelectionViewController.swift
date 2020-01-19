1f890460a5f6e8d6bc58d85fbc38d93dc7a9584c
bharathCTreehouse <bharathc6787@gmail.com>
2020-01-20 00:14:30 +0530

    Initial setup of search criteria for Earth imagery API.
    Initial setup of search criteria for Earth imagery API.

---
+++ UniverseImagery/UniverseEarthCriteriaSelectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UniverseEarthCriteriaSelectionViewController.swift
+//  UniverseImagery
+//
+//  Created by Bharath Chandrashekar on 19/01/20.
+//  Copyright © 2020 Bharath. All rights reserved.
+//
+
+import UIKit
+
+class UniverseEarthCriteriaSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
